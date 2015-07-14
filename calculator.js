99831347ddf1d6e64b657bb2140a19061210badd
elvece <lvcifferello@gmail.com>
2015-07-14 11:56:14 -0600

    added solution to class calculator exercise

---
+++ calculator.js
@@ -0,0 +1,21 @@
+var calc =
+{max: function (arrayNum){
+    var max = arrayNum[0];
+    for ( var i=1; i<arrayNum.length; i++){
+    if (max<arrayNum[i]){
+        max=arrayNum[i];
+      }
+    }
+    return max;
+  },
+
+ avg: function(arrayNum){
+    var sum = arrayNum[0];
+    for ( var i=0; i<arrayNum.length; i++){
+       sum += arrayNum[i];
+    }
+    return sum/arrayNum.length;
+    }
+  };
+
+calc.avg([10, 188, 5, 20, 3, 77]);
